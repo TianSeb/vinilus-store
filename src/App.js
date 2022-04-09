fc661f4dd09fbbf0d8efedc32eaf0d6d9a5fdaf9
AlassTian <sebastian.alasia@kinandcarta.com>
2022-04-09 16:26:14 -0300

    testing deploy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
     <>
       <h1>Vinilus Store</h1>
       <p>Coming Soon!</p>
+      <ul>
+        <li>Testing</li>
+        <li>1 2 3</li>
+        <li>one tu tri</li>
+      </ul>
     </>
   );
 }
